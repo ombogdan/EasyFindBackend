0e75e2347281d10508fa47e54227a4039509a274
ombogdan <omelcenko44@gmail.com>
2024-11-06 21:52:19 +0200

    Add superuser and init project

--- EasyFindBackend/asgi.py
+++ EasyFindBackend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EasyFindBackend1 project.
+ASGI config for EasyFindBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyFindBackend1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyFindBackend.settings')
 
 application = get_asgi_application()
---
+++ myapp/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Люблю тебеееее маниш мене дурманиш")
