ec31701cfbc6ce1f718ccacde2471ddf50fa3505
ombogdan <omelcenko44@gmail.com>
2025-04-14 15:33:57 +0300

    Add changes with photo

--- EasyFindBackend/settings.py
+++ EasyFindBackend/settings.py
@@ -137,3 +137,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 🔥 потрібна а�
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media')
--- EasyFindBackend/urls.py
+++ EasyFindBackend/urls.py
@@ -1,8 +1,9 @@
 # EasyFindBackend/urls.py
 from django.contrib import admin
 from django.urls import path, include
-
 from myapp.views import NearbyOrganizationsView, NearbyServicesView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
     path('api/organizations/nearby/', NearbyOrganizationsView.as_view(), name='nearby_organizations'),
     path("api/nearby-services/", NearbyServicesView.as_view(), name="nearby-services"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
