e0f6977e9155a58e70d7e23c05ea98e14c91345d
ombogdan <omelcenko44@gmail.com>
2025-04-16 22:46:47 +0300

    Change db

--- EasyFindBackend/asgi.py
+++ EasyFindBackend/asgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+import dotenv
+
+dotenv.load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyFindBackend.settings')
 
--- EasyFindBackend/wsgi.py
+++ EasyFindBackend/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import dotenv
+
+dotenv.load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyFindBackend.settings')
 
